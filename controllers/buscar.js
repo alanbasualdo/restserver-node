ce7e8cf7f65f6aac39423bfb41c2780eec9784f5
Alan Basualdo <alanbasualdo7@outlook.com>
2022-06-04 20:54:22 -0300

    4/6

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -21,6 +21,60 @@ const buscarUsuarios = async (termino = '', res = response) => {
         });
     }
 
+    const regex = new RegExp(termino, 'i');
+
+    const usuarios = await Usuario.find({
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+    });
+
+    res.json({
+        results: usuarios
+    })
+
+}
+
+const buscarCategorias = async (termino = '', res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino); // TRUE 
+
+    if (esMongoID) {
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results: (categoria) ? [categoria] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const categorias = await Categoria.find({ nombre: regex, estado: true });
+
+    res.json({
+        results: categorias
+    })
+
+}
+
+const buscarProductos = async (termino = '', res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino); // TRUE 
+
+    if (esMongoID) {
+        const producto = await Producto.findById(termino).populate('categoria', 'nombre');
+        return res.json({
+            results: (producto) ? [producto] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const productos = await Producto.find({ nombre: regex, estado: true })
+        .populate('categoria', 'nombre')
+
+    res.json({
+        results: productos
+    })
+
 }
 
 const buscar = (req, res = response) => {
